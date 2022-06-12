54fb414d30ea7c46376797e55bfdc090b9a48782
David Marsal <davidmarsalivars@gmail.com>
2022-06-12 13:18:35 -0400

    Change field Password

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -26,7 +26,7 @@ class RegistrationFormType extends AbstractType
                     ]),
                 ],
             ])
-            ->add('plainPassword', PasswordType::class, [
+            ->add('Password', PasswordType::class, [
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
                 'mapped' => false,
